8854ba31e92f775f13553beecfb1033b9841f43b
talhakashif72 <talhakashif7213@gmail.com>
2025-01-09 09:18:04 +0500

    print naturel numbers

---
+++ index.js
@@ -0,0 +1,20 @@
+// print all natural number 1 to n.
+
+let i=1;
+let n=5;
+while(i<=n){
+console.log (i)
+i++;
+}
+
+// reverse method
+
+let t=5;
+let w=1;
+while (t>=w) {console.log(t)
+    t--;
+}
+
+
+
+
